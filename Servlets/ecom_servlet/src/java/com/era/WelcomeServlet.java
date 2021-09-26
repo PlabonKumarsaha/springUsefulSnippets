b1947389f15d381c669f04bbb1cafc1ed1801bfa
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2021-09-26 12:22:41 +0600

    post method added,Helloservlet changed to Welcomes

---
+++ Servlets/ecom_servlet/src/java/com/era/WelcomeServlet.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.era;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author pkuma
+ */
+// controller
+// root :http://localhost:8084/ecom/HelloServlet
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+    
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       // super.doGet(req, resp); //To change body of generated methods, choose Tools | Templates.
+       // path : http://localhost:8084/ecom/welcome?name=Plabon
+//       String name =req.getParameter("name");
+//       String dept =req.getParameter("dept");
+//       
+//       
+//       resp.getWriter().write("<h1>Welcome User "+name+"</h1>");
+//       resp.getWriter().write("<h1> From Dept "+dept+"</h1>");
+       
+    }
+
+    /* keeping senindg the message using Get is not good as all the data are 
+     visible. So instead POST is used */
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doPost(req, resp); //To change body of generated methods, choose Tools | Templates.
+       // while sending sesnitive data always use POST
+        String name =req.getParameter("name");
+       String dept =req.getParameter("dept");
+       
+       
+       resp.getWriter().write("<h1>Welcome User "+name+"</h1>");
+       resp.getWriter().write("<h1> From Dept "+dept+"</h1>");
+    }
+    
+
+   
+    
+}
