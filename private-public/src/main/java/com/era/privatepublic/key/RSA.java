3c718daae158a030a17d6f77bc75d9239aaf8c33
Plabon Kumar saha <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-03-03 11:15:54 +0600

    priavte key private key concepts added

--- private-public/src/main/java/com/era/privatepublic/key/RSA.java
+++ private-public/src/main/java/com/era/privatepublic/key/RSA.java
@@ -35,7 +35,7 @@ public class RSA {
 
     public String encrypt(String message) throws Exception{
         byte[] messageToBytes = message.getBytes();
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        Cipher cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding");
         cipher.init(Cipher.ENCRYPT_MODE,publicKey);
         byte[] encryptedBytes = cipher.doFinal(messageToBytes);
         return encode(encryptedBytes);
@@ -46,7 +46,7 @@ public class RSA {
 
     public String decrypt(String encryptedMessage) throws Exception{
         byte[] encryptedBytes = decode(encryptedMessage);
-        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+        Cipher cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding");
         cipher.init(Cipher.DECRYPT_MODE,privateKey);
         byte[] decryptedMessage = cipher.doFinal(encryptedBytes);
         return new String(decryptedMessage,"UTF8");
