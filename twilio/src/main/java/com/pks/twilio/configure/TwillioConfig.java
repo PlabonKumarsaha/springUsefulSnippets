17f454ee2629c2c5e8b7f1758f42dbb90f697963
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-07-06 22:42:56 +0600

    add : lombok added

--- twilio/src/main/java/com/pks/twilio/configure/TwillioConfig.java
+++ twilio/src/main/java/com/pks/twilio/configure/TwillioConfig.java
@@ -1,10 +1,12 @@
 package com.pks.twilio.configure;
 
+import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix = "twilio")
+@Data
 public class TwillioConfig {
 
     private String accountSid;
