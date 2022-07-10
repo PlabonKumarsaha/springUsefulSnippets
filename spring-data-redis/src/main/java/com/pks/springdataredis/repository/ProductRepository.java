46922646fe766c859c7b15fd4554708d4bd2c2cb
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-07-10 09:33:42 +0600

    add : repository added for product

---
+++ spring-data-redis/src/main/java/com/pks/springdataredis/repository/ProductRepository.java
@@ -0,0 +1,38 @@
+package com.pks.springdataredis.repository;
+
+import com.pks.springdataredis.entity.Product;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class ProductRepository {
+
+    // has key was mentioned in entity class
+    public static final String HASH_KEY = "Product";
+    @Autowired
+    private RedisTemplate template;
+
+
+    public Product save(Product product){
+        template.opsForHash().put(HASH_KEY,product.getId(),product);
+        return product;
+    }
+
+    public List<Product> findAll(){
+        return template.opsForHash().values(HASH_KEY);
+    }
+
+    public Product findProductById(int id){
+        return (Product) template.opsForHash().get(HASH_KEY,id);
+    }
+
+    public String deleteProduct(int id){
+        template.opsForHash().delete(HASH_KEY,id);
+        return "product removed !!";
+    }
+
+
+}
