2a4b530a353d01ce8bb5fcd2f1d73265459e649c
pkumarsaha71@gmail.com <46686524+PlabonKumarsaha@users.noreply.github.com>
2022-07-11 14:39:58 +0600

    add : publisher-consumer rabbitmq

---
+++ rabbitmq/src/main/java/com/pks/rabbitmq/consummer/User.java
@@ -0,0 +1,15 @@
+package com.pks.rabbitmq.consummer;
+
+import com.pks.rabbitmq.config.MessagingConfig;
+import com.pks.rabbitmq.dto.OrderStatus;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class User {
+
+    @RabbitListener(queues = MessagingConfig.QUEUE)
+    public void consumeMessageFromQueue(OrderStatus orderStatus) {
+        System.out.println("Message recieved from queue : " + orderStatus);
+    }
+}
